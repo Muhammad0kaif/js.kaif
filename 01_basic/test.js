ff67d4e62534a8a2434a5b70788846b8a8f88bd1
Muhammad0kaif <111998659+Muhammad0kaif@users.noreply.github.com>
2025-12-09 16:54:51 +0000

    objects in js

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,17 @@
-console.log("kaif")
+
+/*
+let a=1
+const b="kaif"
+let c=4
+let d=Number(b)
+
+let str1="Good"
+let str2=" Morning"
+d=str1+str2
+
+console.log(`Hello how are u ${str1} and ${str2}`)
+*/
+const gameNam=new String('kaif')
+console.log(gameNam.toLocaleUpperCase())
+console.log(gameNam.charAt(2))
+console.log(gameNam.indexOf('i'))
